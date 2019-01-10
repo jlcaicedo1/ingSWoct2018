796a9171a23107c3d2794df897aa29e7d730ba5d
JoseLuis <JoseLuis@DESKTOP-SQA8EH0>
2019-01-10 00:36:18 -0500

    Formulario Ingreso de Datos, los resultados son almacenados en un .txt

---
+++ src/Clases/Clase_Registro.java
@@ -0,0 +1,15 @@
+
+package Clases;
+
+public class Clase_Registro {
+    
+    public String nombre;
+    public String apellido;
+    
+    public Clase_Registro (String nom, String ape){
+        
+        this.nombre= nom;
+        this.apellido= ape;
+    }
+    
+}
---
+++ src/Vista/Registrar.java
@@ -0,0 +1,262 @@
+package Vista;
+
+import Clases.Clase_Registro;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+public class Registrar extends javax.swing.JFrame {
+
+    DefaultTableModel ModeloTabla;
+    Clase_Registro Registro;
+    Clase_Registro[] ArregloRegistro;
+    String[] cabecera = {"Nro", "NOMBRE", "APELLIDO"};
+    String[][] data = {};
+    int NumRegistro;
+
+    public Registrar() {
+        initComponents();
+        ArregloRegistro = new Clase_Registro[100];
+        ModeloTabla = new DefaultTableModel(data, cabecera);
+        jTable1.setModel(ModeloTabla);
+        NumRegistro = 0;
+        ExtraerDeArchivo();
+
+    }
+
+    void GrabarEnArchivo(Clase_Registro e) {
+
+        try {
+
+            FileWriter fw = new FileWriter("Registro.txt", true);
+            PrintWriter pw = new PrintWriter(fw);
+            pw.println(e.nombre + " | " + e.apellido);
+            pw.close();
+
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Error al grabar archivo" + ex);
+        }
+    }
+
+    void ExtraerDeArchivo() {
+
+        String linea;
+        try {
+            FileReader fr = new FileReader("Registro.txt");
+            BufferedReader br = new BufferedReader(fr);
+            while ((linea = br.readLine()) != null) {
+
+                StringTokenizer st = new StringTokenizer(linea, " | ");
+                String nom = st.nextToken();
+                String ape = st.nextToken();
+                Registro = new Clase_Registro(nom, ape);
+                ArregloRegistro[NumRegistro] = Registro;
+                NumRegistro++;
+            }
+            br.close();
+
+        } catch (Exception ex) {
+
+            JOptionPane.showMessageDialog(null, "Error al abrir archivo" + ex);
+        }
+    }
+
+    void InsertarEnTabla(Clase_Registro c) {
+
+        Object[] fila = {NumRegistro + 1, Registro.nombre, Registro.apellido};
+        ModeloTabla.addRow(fila);
+    }
+
+    void Registrar() {
+        String a = jTextField1.getText();
+        String b = jTextField2.getText();
+        Registro = new Clase_Registro(a,b);
+        ArregloRegistro[NumRegistro] = Registro;
+        GrabarEnArchivo(Registro);
+        InsertarEnTabla(Registro);
+        NumRegistro++;
+       // Limpiar();
+    }
+
+    void Limpiar() {
+
+        jTextField1.setText("");
+        jTextField2.setText("");
+        jTextField1.requestFocus();
+    }
+
+    String Error() {
+
+        String a = jTextField1.getText();
+        String b = jTextField2.getText();
+        if (a.equals("") && b.equals("")) {
+            return "Ingrese Nombre y Apellido";
+        }
+
+        if (a.equals("")) {
+            return "Ingrese Nombre";
+        }
+        if (b.equals("")) {
+            return "Ingrese Apellido";
+        }
+
+        return "";
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jButton1 = new javax.swing.JButton();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("Registrar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+
+            }
+        ));
+        jScrollPane3.setViewportView(jTable1);
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        jLabel3.setText("Nombre");
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        jLabel4.setText("Apellido");
+
+        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField2ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(95, 95, 95)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE)
+                    .addComponent(jTextField1))
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 468, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(48, 48, 48)
+                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addGap(37, 37, 37)
+                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jButton1)
+                        .addGap(37, 37, 37)))
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 226, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+        if (Error().isEmpty()) {
+            Registrar();
+            JOptionPane.showMessageDialog(null, "OK");
+        } else {
+
+            JOptionPane.showMessageDialog(null, "Error  - " + Error());
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField2ActionPerformed
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Registrar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Registrar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Registrar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Registrar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Registrar().setVisible(true);
+            }
+        }
+        );
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    // End of variables declaration//GEN-END:variables
+}
